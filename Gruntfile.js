4ea1b5e1798ab80a25b17e3dec2f68d93f458540
Bob Wassermann <bob@wassermann.nl>
2015-01-17 03:36:15 +0100

    unfucking

--- Gruntfile.js
+++ Gruntfile.js
@@ -279,7 +279,7 @@ module.exports = function (grunt) {
         files: [{
           src: [
             '<%= yeoman.dist %>/js/**/*.js',
-            '<%= yeoman.dist %>/css/*.css',
+            '<%= yeoman.dist %>/css/**/*.css',
             '<%= yeoman.dist %>/img/**/*.{gif,jpg,jpeg,png,svg,webp}',
             '<%= yeoman.dist %>/fonts/**/*.{eot*,otf,svg,ttf,woff}'
           ]
